af0efcf66a3a392a8d26852cb97474c2b8de0aef
Kirsty White <kirstywhite@Kirstys-MacBook-Air.local>
2019-05-08 12:41:06 +0100

    making first push into the repo

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=eJO5HU_7_1w"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hi my name is slim shady
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
